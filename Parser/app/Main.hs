8f99d6e5d338eac4b9dcdb2b0331f2f80bfa9896
timoteipalade <palade.timotei@gmail.com>
2021-01-15 19:33:57 +0100

    Changes to parser

--- Parser/app/Main.hs
+++ Parser/app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
     
 import Lib
+import Parsing
+
+-- Advent of Code 2020 Problem 4
+
+solvePart1 :: String -> Int
+solvePart1 input = case parse passports input of
+                        [] -> 0
+                        [(p, _)] -> (length . filter (== True) . map hasRequiredFields) p
+
+solvePart2 :: String -> Int
+solvePart2 input = case parse passports input of
+                        [] -> 0
+                        [(p, _)] -> (length . filter (==True) . map isValid) p
 
 main :: IO ()
-main = print "run tests"
+main = do input <- readFile "./input/input4.txt"
+          print (solvePart1 input)
+          print (solvePart2 input)
