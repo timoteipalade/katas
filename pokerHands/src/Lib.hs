cbe21b925a8890b260d59666ce4bd28db4c58844
timoteipalade <palade.timotei@gmail.com>
2021-01-18 14:00:01 +0100

    Clean up and reorg

--- pokerHands/src/Lib.hs
+++ pokerHands/src/Lib.hs
@@ -1,44 +1,17 @@
-module Lib
-    ( someFunc
-    ) where
-
+module Lib() where
 import Control.Applicative
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Hand = [Card]
 
-type Hand = [Card] -- 5 cards only
 data Card = Card {rank :: Rank, suite :: Suite}
-type Rank = Int
+
+type Rank = Int -- between 1 and and 14 (How can I specify this restriction?)
+
 data Suite = Clubs | Diamonds | Hearts | Spades deriving Eq
 
-winningHands :: [Hand] -> [Hand]
-winningHands inp = []
-
--- Thre are 9 categories of hands in standard poker:
--- 1. Highest Card
--- 2. One Pair
--- 3. Two Pair 
--- 4. Three of a kind
--- 5. Straight 
--- 6. Flush
--- 7. Full House
--- 8. Four of a kind
--- 9. Straight flush
-
--- first Rank represents the category, the rest stand for other ranks that depend on the category
--- to compare Scores you need to compare the ranks from left to right
--- if one rank is bigger than the other, then one rank is bigger than the other 
--- if all ranks are the same the Score is the same
-data Score = Score CategoryId Rank Rank Rank Rank Rank
--- TODO: Implement comparison
-
-type CategoryId = Int -- number between 1 and 9
-
+-- Note: To be able to implement the alternative you need to implement Functor and Applicative 
+-- Functor, Applicative, Monad, and Alternative need a metatype (a type that applies to another type.)
 newtype Category a = Category (Hand -> Maybe a)
-
-eval :: Category a -> Hand -> Maybe a
-eval (Category a) hand = a hand
 
 instance Functor Category where
    -- fmap :: (a -> b) -> Parser a -> Parser b
@@ -67,44 +40,26 @@ instance Alternative Category where
                                     Nothing -> eval q hand
                                     Just a -> Just a)
 
--- Note: To be able to implement the alternative you need to implement Functor and Applicative 
--- Functor, Applicative, Monad, and Alternative need a metatype (a type that applies to another type.)
+-- Score 
 
--- Notes on implementation so far. I seem to be a little confused. Category FullRank does not make sense to me. 
+data Score = Score CategoryId Rank Rank Rank Rank Rank
+type CategoryId = Int -- number between 1 and 9
 
---fullRank :: Hand -> Maybe Score
-pokerHand :: Category Score
-pokerHand = do straightFlush <|> fourOfAKind <|> fullHouse <|> flush <|> straight <|> threeOfAKind <|> twoPairs <|> onePair <|> highCard
+pokerHandScore :: Category Score
+pokerHandScore = do straightFlush <|> fourOfAKind <|> fullHouse <|> flush <|> straight <|> threeOfAKind <|> twoPairs <|> onePair <|> highCard
 
--- returns the highest rank, if cards are consecutive
-consecutive :: Hand -> Maybe Rank
-consecutive [] = Nothing 
-consecutive hand = Just 0
+-- Category Scores
 
--- returns the ranks ordered descendingly, if all cards have the same suite
-sameSuite :: Hand -> Maybe [Rank]
-sameSuite [] = Nothing
-sameSuite hand = Just []
-
--- returns the rank of the matching cars, and an array of the ranks of the other non matching cards ordered descendingly, if you have count cards of the same rank
-sameRank :: Rank -> Hand -> Maybe (Rank, [Rank])
-sameRank _ [] = Nothing
-sameRank count hand = Just (0, [])
-
--- returns the ranks of each pair ordered descendingly and the ranks of the remaining cards ordered descendingly, if you have at least one pair
-pairs :: Int -> Hand -> Maybe ([Rank], [Rank])
-pairs _ [] = Nothing
-pairs count hand = Just ([], [])
-
--- returns the highest rank in a hand
-maxRank :: Hand -> Maybe Rank
-maxRank [] = Nothing 
-maxRank hand = Just 0
-
-count :: Int -> Hand -> Maybe Int
-count i hand = if length hand == i then return i else Nothing
-
--- Categories
+-- Thre are 9 categories of hands in standard poker listed here from weakest to strongest:
+-- 1. Highest Card
+-- 2. One Pair
+-- 3. Two Pair 
+-- 4. Three of a kind
+-- 5. Straight 
+-- 6. Flush
+-- 7. Full House
+-- 8. Four of a kind
+-- 9. Straight flush
 
 straightFlush :: Category Score
 straightFlush = Category (\hand -> do count 5 hand
@@ -152,3 +107,38 @@ highCard :: Category Score
 highCard = Category (\hand -> do count 5 hand
                                  highestRank <- maxRank hand
                                  return (Score 1 highestRank 0 0 0 0))
+
+-- Utils
+
+-- returns the highest rank, if cards are consecutive
+consecutive :: Hand -> Maybe Rank
+consecutive [] = Nothing 
+consecutive hand = Just 0
+
+-- returns the ranks ordered descendingly, if all cards have the same suite
+sameSuite :: Hand -> Maybe [Rank]
+sameSuite [] = Nothing
+sameSuite hand = Just []
+
+-- returns the rank of the matching cars, and an array of the ranks of the other non matching cards ordered descendingly, if you have count cards of the same rank
+sameRank :: Rank -> Hand -> Maybe (Rank, [Rank])
+sameRank _ [] = Nothing
+sameRank count hand = Just (0, [])
+
+-- returns the ranks of each pair ordered descendingly and the ranks of the remaining cards ordered descendingly, if you have at least one pair
+pairs :: Int -> Hand -> Maybe ([Rank], [Rank])
+pairs _ [] = Nothing
+pairs count hand = Just ([], [])
+
+-- returns the highest rank in a hand
+maxRank :: Hand -> Maybe Rank
+maxRank [] = Nothing 
+maxRank hand = Just 0
+
+-- returns the count if the length of the hand matches it
+count :: Int -> Hand -> Maybe Int
+count i hand = if length hand == i then return i else Nothing
+
+-- evaluates a category
+eval :: Category a -> Hand -> Maybe a
+eval (Category a) hand = a hand
