9f8b9a2712c1a7ca3d7240061a1a6454bbd04b4b
timoteipalade <palade.timotei@gmail.com>
2021-02-15 16:00:46 +0100

    Data Types and Solution Type

--- DesignOfComputerProgramsPeterNorvig/Lesson2/zebraPuzzle/src/Lib.hs
+++ DesignOfComputerProgramsPeterNorvig/Lesson2/zebraPuzzle/src/Lib.hs
@@ -4,3 +4,17 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- Data Types
+data Nationality = Norwegian | Japanese | English | Spanish | Ukranian
+data Animal = Dog | Snail | Fox | Horse | Zebra
+data Drink = Tea | Coffee | Milk | Juice | Water
+data Cigarette = Gold | Chester | Kools | Lucky | Parliments
+data Color = Yellow | Green | Ivory | Red | Blue
+
+-- Solution Type 
+type Column = (Nationality, Animal, Drink, Cigarette, Color)
+type Solution = [Column]
+
+-- Constraint Type
+
