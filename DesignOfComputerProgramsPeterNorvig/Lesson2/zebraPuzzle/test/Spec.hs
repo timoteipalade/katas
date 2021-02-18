8721dfd1c63eb82969d19990371c4f3999ff9872
timoteipalade <palade.timotei@gmail.com>
2021-02-18 11:59:03 +0100

    Add benchmark

--- DesignOfComputerProgramsPeterNorvig/Lesson2/zebraPuzzle/test/Spec.hs
+++ DesignOfComputerProgramsPeterNorvig/Lesson2/zebraPuzzle/test/Spec.hs
@@ -1,11 +1,19 @@
 import Lib
+import Criterion.Main
+
+benchmark = 
+    defaultMain [
+        bgroup "Zebra Puzzle" [bench "Zebra Puzzle benchmark" $ whnfIO (putStrLn $ show run)]
+    ]
 
 main :: IO ()
-main = do 
+main = 
+    do 
         print run
         print (length firstLevel)
         print (length secondLevel)
         print (length thirdLevel)
         print (length fourthLevel)
         print (length fifthLevel)
+        -- benchmark
         
